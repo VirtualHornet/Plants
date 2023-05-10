d77016256805aac457ea7c6ae8911dd3ba967061
VirtualHornet <attilacelluska@gmail.com>
2023-05-10 17:06:51 +0200

    update
    Signed-off-by: VirtualHornet <attilacelluska@gmail.com>

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -4,7 +4,12 @@ import Tree from "./Tree";
 
 function Home (){
     return(<Cointainer>
+
+      <h1>PLANTS</h1>
+      <p>The PLANTS Database provides standardized information about the vascular plants, mosses, liverworts, hornworts, and lichens of the U.S. and its territories.</p>
       <Flower />
+
+      
 
       <Tree />
 
@@ -16,5 +21,19 @@ function Home (){
 }
 const Cointainer = styled.div`
   color: #fff;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  h1{
+    font-size: 4rem;
+    margin-left: auto;
+    margin-right: auto;
+  }
+  p{
+    width: 60%;
+    text-align: center;
+    margin-left: auto;
+    margin-right: auto;
+  }
 `
 export default Home;
